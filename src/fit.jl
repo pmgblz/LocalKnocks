03a9e94739bb64c3c5d7217adf74620498d1fb84
Benjamin Chu <benchu99@hotmail.com>
2025-01-15 17:03:17 -0800

    prototype code for second order group knockoff for GWAS

--- src/fit.jl
+++ src/fit.jl
@@ -11,11 +11,16 @@ Does GWAS for all SNPs window-by-window, assuming windows are independent.
 # end
 # finally: knockoff filter
 """
-function gwas(data::GWASData; window_width::Int=1000)
+function gwas(
+        data::GWASData; 
+        window_width::Int=1000, 
+        m::Int = 1
+    )
     # create windows
     windows = div(data.p, window_width)
     lambdas = estimate_lambdas(data)
-    beta = Float64[]
+    betas = Float64[]
+    groups = String[]
 
     @showprogress for window in 1:windows
         # SNPs in current window
@@ -25,9 +30,29 @@ function gwas(data::GWASData; window_width::Int=1000)
 
         # initialize data structure and fit
         data_w = initialize(data, idx)
-        beta_w = fit(data_w, lambdas = lambdas)
-        # append!(beta, beta_w)
+        beta_w = fit(data_w, lambdas)
+        append!(betas, beta_w)
+
+        # keep track of groups
+        for g in data_w.groups
+            push!(groups, "window$(window)_group$(g)_0") # the last _0 implies this is original variable
+        end
+
+        # group membership of knockoff variables
+        for k in 1:m, g in data_w.groups
+            push!(groups, "window$(window)_group$(g)_$k")
+        end
     end
+
+    @assert length(betas) == length(groups) "Beta and group vector length differs!"
+
+    # knockoff filter
+    qvalues = filter(betas, groups)
+
+    # assemble dataframe to return
+    df = assemble_result(data, qvalues, groups)
+
+    return df
 end
 
 """
@@ -35,9 +60,19 @@ end
 
 Fits a Lasso regression on `GroupKnockoff` struct using the lambda sequence 
 specified in `lambdas`.
+
+# TODO
+Optimize memory usage
 """
 function fit(data::GroupKnockoff, lambdas::Vector{T}) where T
-    # todo
+    # form design matrix, see TODO
+    Xfull = hcat(data.x, data.xko)
+
+    # lasso
+    path = glmnet(Xfull, y, lambda = lambdas)
+    beta = path.betas[:, end]
+
+    return beta
 end
 
 """
@@ -78,4 +113,80 @@ function estimate_lambdas(data::GWASData; kappa_lasso = 0.6, m::Int = 1)
     lambda_path = vcat(lambda_path[findall(x -> x > lambda, lambda_path)], lambda)
 
     return lambda_path
+end
+
+"""
+    filter(beta::AbstractVector, groups::AbstractVector)
+
+Performs (within-group) knockoff filter. The `beta` vector is assumed to have
+estimated effect sizes for both the original variable and the knockoff variables. 
+Variables are distinguished based on `groups` vector, where 
+"""
+function filter(beta::AbstractVector, groups::AbstractVector)
+    original_idx = findall(x -> endswith(x, "_0"), groups)
+    T0 = beta[original_idx]
+    Tk = [beta[findall(x -> endswith(x, "_$k"), groups)] for k in 1:m]
+    kappa, tau, W = MK_statistics(T0, Tk)
+    qvalues = get_knockoff_qvalue(kappa, tau, m, groups=groups)
+    return qvalues
+end
+
+"""
+    assemble_result(data::GWASData, qvalues::AbstractVector, groups::AbstractVector)
+
+Create a dataframe to store the final output of `gwas`.
+"""
+function assemble_result(
+        data::GWASData, 
+        qvalues::AbstractVector,
+        groups::AbstractVector
+    )
+    df = copy(data.x.snp_info)
+    df[!, "qvalues"] = qvalues
+    df[!, "groups"] = groups
+    return df
+end
+
+"""
+    get_knockoff_qvalue(κ, τ, m, [groups], [rej_bounds])
+
+Computes the knockoff q-value for each variable. The knockoff q-value is the 
+minimum target FDR for a given variable to be selected. For details, see eq 19 
+of https://www.nature.com/articles/s41467-022-34932-z and replace the 
+knockoff-filter by the within-group knockoff filter proposed in Alg2 of "A
+Powerful and Precise Feature-level Filter using Group Knockoffs" by Gu and He (2024).
+
+Note: Code is directly translated from Zihuai's R code here:
+https://github.com/biona001/ghostknockoff-gwas-reproducibility/blob/main/he_et_al/GKL_RunAnalysis_All.R#L36
+"""
+function get_knockoff_qvalue(κ::AbstractVector, τ::AbstractVector, m::Int;
+    groups::AbstractVector=collect(1:length(τ)), rej_bounds::Int=10000
+    )
+    b = sortperm(τ, rev=true)
+    c_0 = κ[b] .== 0
+    offset = 1 / m
+    # calculate ratios for top rej_bounds tau values
+    ratio = Float64[]
+    temp_0 = 0
+    for i in eachindex(b)
+        temp_0 = temp_0 + c_0[i]
+        temp_1 = i - temp_0
+        G_factor = maximum(values(countmap(groups[b][1:i])))
+        temp_ratio = (offset*G_factor+offset*temp_1) / max(1, temp_0)
+        push!(ratio, temp_ratio)
+        i > rej_bounds && break
+    end
+    # calculate q values for top rej_bounds values
+    qvalues = ones(length(τ))
+    if any(x -> x > 0, τ)
+        index_bound = maximum(findall(τ[b] .> 0))
+        for i in eachindex(b)
+            temp_index = i:min(length(b), rej_bounds, index_bound)
+            length(temp_index) == 0 && continue
+            qvalues[b[i]] = minimum(ratio[temp_index])*c_0[i]+1-c_0[i]
+            i > rej_bounds && break
+        end
+        qvalues[qvalues .> 1] .= 1
+    end
+    return qvalues
 end
